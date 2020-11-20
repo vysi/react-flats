5830b148e75cae1bb46fcecc321e37c091fdb36a
Veysi Akin <weysi.akin1@gmail.com>
2020-11-20 15:29:27 +0100

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+// external modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// internal modules
+import '../assets/stylesheets/application.scss';
+import App from './components/app';
+
+// render an instance of the component in the DOM
+ReactDOM.render(<App />, document.querySelector('#root'));
